7f3b143915faadafdd6ba2ba6fffc9f199854b70
eramon92 <estebanramon92@hotmail.com>
2016-10-07 10:43:37 -0500

    Second commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res){
-	res.send({'salud': 'hola'});
+	res.send("Hello world");
 });
 
 app.get('/esteban',function(req,res){
